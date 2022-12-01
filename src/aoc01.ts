9af6302db73a15b07700c9b855c16a60d101a05a
Markus Anetsberger <markus.anetsberger@bmw.de>
2022-12-01 20:42:51 +0100

    added day 1

--- src/aoc01.ts
+++ src/aoc01.ts
@@ -1,14 +1,11 @@
-import  { readNumberList, readStringList, readStringBlock } from './importer';
+import  { readStringBlock } from './importer';
 
-export function aoc_part1(): any {
-    const input = readNumberList('./assets/aoc01.txt');
+export function aoc_part1(): number { return getCaloryArray().slice(-1)[0]; }
+export function aoc_part2(): number { return getCaloryArray().slice(-3).reduce((a, b) => a + b); }
 
-    return input;
+function getCaloryArray(): number[] {
+    return (readStringBlock('./assets/aoc01.txt') as any[])
+            .map((a) => a.split(/\r?\n/).map((a:string) => parseInt(a, 10))
+            .reduce((a: string, b: string) => a + b))
+            .sort((a, b) => a - b);
 }
-
-export function aoc_part2(): any {
-    const input = readNumberList('./assets/aoc01.txt');
-
-    return input;
-}
-
